6bd60a08a3100e9456aae9258b07d4fd0b809702
wolfgang <wolfgang.loeffler@gmail.com>
2018-06-07 21:09:41 +0200

    swipe between views, cleanup

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # WebRemoteControl
-Remote control the mouse and keyboard of a Mac/Windows/Linux computer from a html page, e.g. from a smartphone or a tablet. Adapted from https://github.com/simquat/LocalTrackpad.
+Remote control the mouse and keyboard of a Mac/Windows/Linux computer from a html page, e.g. from a smartphone or a tablet.
+Adapted from https://github.com/simquat/LocalTrackpad.
 
-The views:
+The views (use menu or swipe top rows to change):
 
 - Mouse/trackpad control, cursor, generic keys (youtube, netflix)
 - VLC control buttons
@@ -15,12 +16,14 @@ Note that there is no access control, everybody on the (local) network has acces
     * [Download the jar](https://bitbucket.org/wolfgang/webremotecontrol/downloads)
     * Double-click to run the jar or do `java -jar webremotecontrol.jar` in a terminal.
     * Click on one of the buttons to show a QR code that contains the server URL and scan this with the client.
-    * Adding an icon to your homescreen should make a web app.
+* Client: Adding an icon to your homescreen should make a web app.
+
 
 ![Screenshot_20170205-121604.png](https://bitbucket.org/repo/AxyGpB/images/1468820713-Screenshot_20170205-121604.png)
+(outdated)
 
 # Config file
-An optional config file can be placed next to the jar, see webremotecontrol. Example content:
+An optional config file can be placed next to the jar. Example content:
 
     httpserverport=9000
     websocketport=9001
@@ -32,21 +35,16 @@ An optional config file can be placed next to the jar, see webremotecontrol. Exa
 * check out the code (`git clone ...` or download a zip) 
 * I use the free community version of [IntelliJ IDEA](https://www.jetbrains.com/idea/download/) with the scala 
 plugin for development, just open the project to get started.
-
-Package WebRemoteControl:
-
-* Install the [Scala Build Tool](http://www.scala-sbt.org/)
 * Compile and run manually: `sbt run`
 * Package jar: `sbt dist`. The resulting jar is in `target/`
-
-### Suggestions, bug reports, pull requests, contact ###
-Please use the bitbucket-provided tools for bug reports and contributed code. Anything is welcome!
 
 # Used technologies
 
 * [Scala](http://www.scala-lang.org) and [Scala Build Tool](http://www.scala-sbt.org)
+* [Scala Build Tool](http://www.scala-sbt.org/)
 * [sbt-buildinfo](https://github.com/sbt/sbt-buildinfo)
 * [sbt-assembly](https://github.com/sbt/sbt-assembly)
 * [Java WebSockets](https://github.com/TooTallNate/Java-WebSocket)
 * [Hammer.JS](http://hammerjs.github.io/) to capture touch events
 * [QRGen](https://github.com/kenglxn/QRGen) to generate the QR code
+* [js-mobile-console](https://github.com/B1naryStudio/js-mobile-console) for debugging
--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,59 +1,90 @@
-window.onload = function(){
-    var socket = new WebSocket("ws://" + window.location.hostname + ":8001");
-    var hammer = null;
-    var queue = [];
-    var FACTOR = 4.0;
 
-    loadtrackpad = function(){
-      var trackpad = document.getElementById('cont-trackpadarea');
-      var scroll;
-      var secondaryTap;
-      var prevDeltaX = 0;
-      var prevDeltaY = 0;
-      var scrollAmount = 0;
-      var prevScrollAmount = 0;
-      var canEmit = true;
+var socket = null;
+var hammer = null;
+var hammertr = null;
+var queue = [];
+var FACTOR = 4.0;
+var intervalid = null;
 
-      setInterval(function(){
-        if (socket.readyState != socket.OPEN) {
-            setTimeout(function(){
-                window.location.reload(true);
-            }, 100)
+function debug(s) {
+    console.log(s);
+}
+
+function menuchanged() {
+    'use strict';
+    var vis = document.getElementsByClassName('_vis_');
+    var targetid = document.getElementById('menu').value;
+    var target = document.getElementById(targetid);
+    if (vis.length !== 0) {
+        vis[0].className = vis[0].className.replace('_vis_','_inv_');
+    }
+    if (target !== null ) {
+        target.className = target.className.replace('_inv_','_vis_');
+        var tpa = document.getElementById("cont-trackpadarea");
+        if (targetid == "cont-files") {
+            tpa.className = tpa.className.replace('_vis_','_inv_');
+        } else {
+            tpa.className = tpa.className.replace('_inv_','_vis_');
+            loadtrackpad();
         }
+        if (targetid == "cont-files") {
+            queue.push("fbgetfiles")
+        }
+    }
+}
 
-        var toEmit = queue.shift();
-        if (toEmit) { socket.send(toEmit); }
-      },25);
+function incdecmenu(increment) {
+    var menu = document.getElementById('menu')
+    var newidx = menu.selectedIndex + (increment ? 1 : -1);
+    if (newidx >= menu.length) newidx = 0;
+    if (newidx < 0) newidx = menu.length - 1;
+    menu.selectedIndex = newidx;
+    menuchanged();
+}
 
-      if (hammer !== null) {
-        socket.send('debug,destroying existing hammer...');
-        hammer.destroy();
-      }
+function loadswiper() {
+    var swipediv = document.getElementById('swipecontent');
+    if (hammertr !== null) { hammertr.destroy(); }
+    hammertr = new Hammer(swipediv, { recognizers: [ [Hammer.Swipe,{ direction: Hammer.DIRECTION_HORIZONTAL }]] });
+    hammertr.on('swipeleft', function (ev) {incdecmenu(true)});
+    hammertr.on('swiperight', function (ev) {incdecmenu(false)});
+}
 
-      hammer = Hammer(trackpad);
+function loadtrackpad() {
+    var trackpad = document.getElementById('cont-trackpadarea');
+    var scroll;
+    var secondaryTap;
+    var prevDeltaX = 0;
+    var prevDeltaY = 0;
+    var scrollAmount = 0;
+    var prevScrollAmount = 0;
+    var canEmit = true;
 
-      scroll = new Hammer.Pan({
+    if (hammer !== null) { hammer.destroy(); }
+    hammer = Hammer(trackpad);
+
+    scroll = new Hammer.Pan({
         event: 'scroll',
         direction: Hammer.DIRECTION_VERTICAL,
         pointers: 2
-      });
+    });
 
-      drag = new Hammer.Pan({
+    drag = new Hammer.Pan({
         event: 'drag',
         direction: Hammer.DIRECTION_VERTICAL,
         pointers: 3
-      });
+    });
 
-      secondaryTap = new Hammer.Tap({
+    secondaryTap = new Hammer.Tap({
         event: 'secondaryTap',
         pointers: 2
-      });
+    });
 
-      hammer.add(scroll);
-      hammer.add(drag);
-      hammer.add(secondaryTap);
+    hammer.add(scroll);
+    hammer.add(drag);
+    hammer.add(secondaryTap);
 
-      function panDrag(ev) {
+    function panDrag(ev) {
         dx = Math.round(FACTOR * (ev.deltaX - prevDeltaX));
         dy = Math.round(FACTOR * (ev.deltaY - prevDeltaY));
 
@@ -61,125 +92,69 @@ window.onload = function(){
         prevDeltaY = ev.deltaY;
 
         if(canEmit){
-          queue.push('move,' + dx + ',' + dy);
-          canEmit = false;
-          setTimeout(function(){ canEmit = true; }, 50);
+            queue.push('move,' + dx + ',' + dy);
+            canEmit = false;
+            setTimeout(function(){ canEmit = true; }, 50);
         }
-      }
+    }
 
-      hammer.on('pan', panDrag);
+    hammer.on('pan', panDrag);
 
-      hammer.on('scroll', function(ev){
+    hammer.on('scroll', function(ev) {
         scrollAmount = ev.deltaY + (prevScrollAmount*-1);
         prevScrollAmount = ev.deltaY;
         if(canEmit){
-          queue.push('scroll,' + scrollAmount);
-          canEmit = false;
-          setTimeout(function(){ canEmit = true; }, 50);
+            queue.push('scroll,' + scrollAmount);
+            canEmit = false;
+            setTimeout(function() { canEmit = true; }, 50);
         }
-      });
+    });
 
-      hammer.on('dragstart', function(ev) {
+    hammer.on('dragstart', function(ev) {
         queue.push('dragStart');
-      });
+    });
 
-      hammer.on('drag', panDrag);
+    hammer.on('drag', panDrag);
 
-      hammer.on('dragend', function(ev) {
+    hammer.on('dragend', function(ev) {
         queue.push('dragEnd');
-      });
+    });
 
-      hammer.on('scrollend', function(ev) {
+    hammer.on('scrollend', function(ev) {
         prevScrollAmount = 0;
-      });
+    });
 
-      hammer.on('panend', function(ev) {
+    hammer.on('panend', function(ev) {
         prevDeltaX = 0;
         prevDeltaY = 0;
-      });
+    });
 
-      hammer.on('tap', function(ev) {
+    hammer.on('tap', function(ev) {
         queue.push('tap');
-      });
+    });
 
-      hammer.on('secondaryTap', function(ev) {
+    hammer.on('secondaryTap', function(ev) {
         queue.push('tap2');
-      });
-    };
-
-
-    /////////////////// events
-
-    document.getElementById('textinput').onkeydown = function(event) {
-      if (event.keyCode == 13) {
-        queue.push("text," + document.getElementById('textinput').value);
-      }
-    }
-
-    function pushclosure(a, b) { return function () { queue.push(a + "," + b) }; }
-
-    autos = document.getElementsByClassName('bauto')
-    for (i = 0; i < autos.length; i++) {
-      var myid = autos[i].id;
-      autos[i].addEventListener('click', pushclosure("bauto", myid), false);
-    }
-
-    document.getElementById('menu').addEventListener('change', function () {
-        'use strict';
-        var vis = document.getElementsByClassName('_vis_');
-        var targetid = this.value;
-        console.log("targetid=" + targetid);
-        var target = document.getElementById(targetid);
-        if (vis.length !== 0) {
-            vis[0].className = vis[0].className.replace('_vis_','_inv_');
-        }
-        if (target !== null ) {
-            target.className = target.className.replace('_inv_','_vis_');
-            var tpa = document.getElementById("cont-trackpadarea");
-            if (targetid == "cont-files") {
-                console.log("files!!!");
-                tpa.className = tpa.className.replace('_vis_','_inv_');
-            } else {
-                tpa.className = tpa.className.replace('_inv_','_vis_');
-                loadtrackpad();
-            }
-            if (this.value == "cont-files") {
-                queue.push("fbgetfiles")
-            }
-        }
     });
+}
 
-    document.getElementById('cmd').addEventListener('change', function () {
-        'use strict';
-        queue.push("cmd," + document.getElementById("cmd").value)
-        document.getElementById("cmd").selectedIndex = 0;
-    });
+// init websocket. call after connection loss.
+function initwebsocket() {
+    socket = new WebSocket("ws://" + window.location.hostname + ":8001");
 
-    /////////////////// file browser
-    document.getElementById('fbup').addEventListener('click', function() {
-        queue.push("fbup")
-    }, false);
-
-    document.getElementById('fbtable').addEventListener('click', function (e) {
-        var target = e.target,
-            row, col, rX, cX;
-        if (target.type !== 'button') {return;}
-        col = target.parentElement;
-        row = col.parentElement;
-        rX = row.rowIndex;
-        console.log(rX);
-        queue.push("fbopen," + rX)
-    });
-
-    /////////////////// startup
-
-    // this assumes that the trackpad div is shown initially!
     socket.onopen = function(){
         loadtrackpad();
+        loadswiper();
+        debug("socket.onopen: initialized!");
     }
 
-    /////////////////// react
+    socket.onclose = function(){
+        debug("socket.onclose");
+        clearInterval(intervalid);
+        setTimeout(function(){ initwebsocket(); }, 250);
+    }
 
+    // react
     socket.onmessage = function (event) {
         ss = event.data.split(",");
         switch(ss[0]) {
@@ -204,4 +179,64 @@ window.onload = function(){
                 break;
         }
     }
+
+    intervalid = setInterval(function(){
+        if (socket.readyState == socket.OPEN) {
+             var toEmit = queue.shift();
+             if (toEmit) { socket.send(toEmit); }
+        }
+    },25);
+}
+
+
+window.onload = function() {
+
+    // https://github.com/B1naryStudio/js-mobile-console
+    mobileConsole.show();
+    mobileConsole.options({ showOnError: true, proxyConsole: false, isCollapsed: true, catchErrors: true });
+    mobileConsole.toggleCollapsed();
+
+    initwebsocket();
+
+    // events
+    document.getElementById('textinput').onkeydown = function(event) {
+        if (event.keyCode == 13) {
+            queue.push("text," + document.getElementById('textinput').value);
+        }
+    }
+
+    function pushclosure(a, b) { return function () { queue.push(a + "," + b) }; }
+
+    autos = document.getElementsByClassName('bauto')
+    for (i = 0; i < autos.length; i++) {
+        var myid = autos[i].id;
+        autos[i].addEventListener('click', pushclosure("bauto", myid), false);
+    }
+
+    document.getElementById('menu').addEventListener('change', function() {
+        menuchanged();
+    });
+
+    document.getElementById('cmd').addEventListener('change', function () {
+        'use strict';
+        queue.push("cmd," + document.getElementById("cmd").value)
+        document.getElementById("cmd").selectedIndex = 0;
+    });
+
+    // file browser
+    document.getElementById('fbup').addEventListener('click', function() {
+        queue.push("fbup")
+    }, false);
+
+    document.getElementById('fbtable').addEventListener('click', function (e) {
+        var target = e.target,
+            row, col, rX, cX;
+        if (target.type !== 'button') {return;}
+        col = target.parentElement;
+        row = col.parentElement;
+        rX = row.rowIndex;
+        queue.push("fbopen," + rX)
+    });
+
+    debug("window.onload: initialized!");
 }
